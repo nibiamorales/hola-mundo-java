83c5f230e180304c901fb9a98eac16baf4bb1d6a
Nibia Morales <nibia.morales@uabc.edu.mx>
2025-08-30 21:59:49 -0600

    Primer commit: Programa Hola Mundo en Java

---
+++ src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java!");
+    }
+}
