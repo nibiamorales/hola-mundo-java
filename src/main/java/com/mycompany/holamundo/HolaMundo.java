246cdfe0c9b343c9ce44169dc0f18a2bf29502e2
Nibia Morales <nibia.morales@uabc.edu.mx>
2025-08-30 22:34:26 -0600

    Modificacion para desplegar mi nombre

--- src/main/java/com/mycompany/holamundo/HolaMundo.java
+++ src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -7,5 +7,7 @@ package com.mycompany.holamundo;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo desde Java!");
+        System.out.println("Mi nombre es Nibia Morales!");
+
     }
 }
